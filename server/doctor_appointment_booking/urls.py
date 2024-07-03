0f54318b219ed094184392be09dd3dfc516290d5
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-07-03 21:37:52 +0330

    fix: registering django debug toolbar urls

--- server/doctor_appointment_booking/urls.py
+++ server/doctor_appointment_booking/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
-from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
-    urlpatterns += debug_toolbar_urls()
+    urlpatterns += [path("__debug__/", include("debug_toolbar.urls"))]
