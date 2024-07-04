9761535dadfb3105664128bc4ebfaba26013a4c1
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-07-04 23:55:58 +0330

    refactor: celery module

--- server/doctor_appointment_booking/celery.py
+++ server/doctor_appointment_booking/celery.py
@@ -6,11 +6,6 @@ os.environ.setdefault(
     'doctor_appointment_booking.settings'
 )
 
-os.environ.setdefault(
-    'DJANGO_SETTINGS_MODULE',
-    os.environ.get('DJANGO_SETTINGS_MODULE')
-)
-
 app = Celery('doctor_appointment_booking')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
