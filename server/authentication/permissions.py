0b66fab9c91b318c9ba853648272e786c3345078
Mohammad222PR <mohammades13851@gmail.com>
2024-07-06 22:00:46 +0330

    feat: creating new IsAdminOrReadOnly permission class

--- server/authentication/permissions.py
+++ server/authentication/permissions.py
@@ -4,3 +4,13 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 class IsDoctor(BasePermission):
     def has_permission(self, request, view):
         return request.user.is_doctor
+
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+
+        if request.user.is_authenticated:
+            return request.user.is_staff
+        return False
