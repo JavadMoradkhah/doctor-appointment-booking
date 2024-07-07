4580a21f6daf67e42227d531481a4ca5bc089f18
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-07-07 13:48:11 +0330

    feat: set the fixtures directory path

--- server/doctor_appointment_booking/settings/common.py
+++ server/doctor_appointment_booking/settings/common.py
@@ -120,9 +120,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+FIXTURE_DIRS = [BASE_DIR / 'fixtures']
 
 # Django REST Framework Settings
 
