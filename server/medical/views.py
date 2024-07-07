8707d44bf65a00b34f45b88461893878308e0b1a
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-07-07 20:21:29 +0330

    fix: the medical app views bugs

--- server/medical/views.py
+++ server/medical/views.py
@@ -1,26 +1,21 @@
 from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.exceptions import APIException
+from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
 from authentication.permissions import IsAdminOrReadOnly, IsPatient
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Province, City, Insurance, UserInsurance
 from . import serializers
-from rest_framework.response import Response
-from rest_framework.permissions import SAFE_METHODS
-from rest_framework.exceptions import APIException
-# Create your views here.
 
 
 class ProvinceViewSet(viewsets.ModelViewSet):
-    permission_classes = [
-        IsAuthenticatedOrReadOnly,
-        IsAdminOrReadOnly
-    ]
     queryset = Province.objects.all()
     serializer_class = serializers.ProvinceSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class CityViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly, IsAdminOrReadOnly]
     queryset = City.objects.all()
+    permission_classes = [IsAdminOrReadOnly]
 
     def get_serializer_class(self):
         if self.request.method not in SAFE_METHODS:
@@ -32,25 +27,25 @@ class ProvinceCitiesViewSet(viewsets.ViewSet):
     serializer_class = serializers.ProvinceCitiesListSerializer
 
     def list(self, request, province_pk):
-        cities = City.objects.filter(province__id=province_pk).all()
+        cities = City.objects.filter(province_id=province_pk).all()
         serializer = self.serializer_class(cities, many=True)
         return Response(serializer.data)
 
 
 class InsuranceViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly, IsAdminOrReadOnly]
-    serializer_class = serializers.InsuranceSerializer
     queryset = Insurance.objects.all()
+    serializer_class = serializers.InsuranceSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class UserInsuranceViewSet(viewsets.ModelViewSet):
-    permission_classes = [IsAuthenticatedOrReadOnly, IsPatient]
+    permission_classes = [IsAuthenticated, IsPatient]
     serializer_class = serializers.UserInsuranceSerializer
 
     def get_queryset(self):
-        queryset = UserInsurance.objects.filter(
-            user_id=self.request.user.id).all()
-        return queryset
+        return UserInsurance.objects.filter(
+            user_id=self.request.user.id
+        ).all()
 
     def get_serializer_context(self):
         return {
@@ -60,6 +55,8 @@ class UserInsuranceViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         if UserInsurance.objects.filter(user_id=request.user.id).count() >= 2:
             raise APIException(
-                detail='امکان ثبت بیشتراز 2 بیمه را ندارید', code=status.HTTP_400_BAD_REQUEST)
+                detail='امکان ثبت بیشتراز 2 بیمه وجود ندارد',
+                code=status.HTTP_400_BAD_REQUEST
+            )
 
         return super().create(request, *args, **kwargs)
