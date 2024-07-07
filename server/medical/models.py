5eaf66f11ebc1fa8c64dbfa123b401657dd2b9ae
Mohammad222PR <mohammades13851@gmail.com>
2024-07-07 17:01:21 +0330

    feat: import and use get_user_model function

--- server/medical/models.py
+++ server/medical/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from . import validators
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User = get_user_model()
 
 
 class Province(models.Model):
