ed1b8ac54e62874eb364ecd533cca933ed31f81f
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-07-08 15:14:16 +0330

    refactor: the medical app URLConf module

--- server/medical/urls.py
+++ server/medical/urls.py
@@ -6,41 +6,19 @@ urlpatterns = []
 
 router = DefaultRouter()
 
-router.register(
-    r'provinces',
-    views.ProvinceViewSet,
-    basename='provinces'
-)
+router.register(r"provinces", views.ProvinceViewSet, basename="provinces")
 
-router.register(
-    r'cities',
-    views.CityViewSet,
-    basename='cities'
-)
+router.register(r"cities", views.CityViewSet, basename="cities")
 
-router.register(
-    r'insurance',
-    views.InsuranceViewSet,
-    basename='insurance'
-)
+router.register(r"insurance", views.InsuranceViewSet, basename="insurance")
 
-router.register(
-    r'my-insurances',
-    views.UserInsuranceViewSet,
-    basename='my-insurances'
-)
+router.register(r"my-insurances", views.UserInsuranceViewSet, basename="my-insurances")
 
-province_router = routers.NestedSimpleRouter(
-    router,
-    r'provinces',
-    lookup='province'
-)
+router.register(r"facilities", views.FacilityViewSet, basename="facilities")
 
-province_router.register(
-    r'cities',
-    views.ProvinceCitiesViewSet,
-    basename='cities'
-)
+province_router = routers.NestedSimpleRouter(router, r"provinces", lookup="province")
+
+province_router.register(r"cities", views.ProvinceCitiesViewSet, basename="cities")
 
 urlpatterns += router.urls
 
