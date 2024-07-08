4f2fda2fe3d503b2513047a57b2c753c2a37d7c7
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-07-08 21:21:19 +0330

    feat: create the IsUserOwner permission class

--- server/authentication/permissions.py
+++ server/authentication/permissions.py
@@ -16,9 +16,19 @@ class IsPatient(BasePermission):
         return request.user.is_patient
 
 
+class IsUserOwner(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.id == int(view.kwargs["pk"])
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.id == obj.user_id
+
+
 class IsAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
         if request.method in SAFE_METHODS:
             return True
 
-        return bool(request.user and request.user.is_authenticated and request.user.is_staff)
+        return bool(
+            request.user and request.user.is_authenticated and request.user.is_staff
+        )
