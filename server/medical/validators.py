6c2c67857eac26263ee9094080dcd2361446d9fe
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2024-07-11 16:32:43 +0330

    refactor: the medical app validators module

--- server/medical/validators.py
+++ server/medical/validators.py
@@ -4,16 +4,16 @@ from django.core.exceptions import ValidationError
 
 
 class TelephoneValidator(RegexValidator):
-    regex = r'^([0-9]{11})$'
-    message = 'یک شماره تلفن معتبر وارد کنید'
+    regex = r"^([0-9]{11})$"
+    message = "یک شماره تلفن معتبر وارد کنید"
 
 
 def validate_image(image):
     extensions = pathlib.Path(image.name).suffix
-    allow_extensions = ['.jpg', '.jpeg']
+    allow_extensions = [".jpg", ".jpeg"]
+
     if not extensions in allow_extensions:
-        raise ValidationError(
-            f'({allow_extensions})فرمت ارسال شده معتبر نمی باشد فرمت های مجاز')
-    if image.size > 2 * 1024 ** 2:
-        raise ValidationError(
-            f'سایز تصویر مجاز نمی باشد  سایز مجاز 2 مگابایت می باشد')
+        raise ValidationError(f"مجاز هستند {' و '.join(allow_extensions)} فقط فرمت های")
+
+    if image.size > 2 * 1024 * 1024:
+        raise ValidationError(f"حجم تصویر نباید بیش از ۲ مگابایت باشد")
