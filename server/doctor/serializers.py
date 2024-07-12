7ea47efe5e0187887aee2c6ec378564ce80714af
Mohammad222PR <mohammades13851@gmail.com>
2024-07-12 13:33:19 +0330

    feat: create DoctorScheduleListRetrieveSerializer and DoctorScheduleCreateUpdateSerializer class

--- server/doctor/serializers.py
+++ server/doctor/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
-from .models import Doctor, DoctorDegree, Specialization
+from medical.models import MedicalCenterSchedule
 from account.models import Profile
+from .models import Doctor, DoctorDegree, Specialization, DoctorSchedule
 from . import validators
 
 
@@ -13,7 +14,6 @@ class DoctorListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
         fields = [
-            "id",
             "first_name",
             "last_name",
             "specialization",
@@ -33,7 +33,6 @@ class DoctorRetrieveSerializer(serializers.ModelSerializer):
     class Meta:
         model = Doctor
         fields = [
-            "id",
             "first_name",
             "last_name",
             "specialization",
@@ -47,7 +46,6 @@ class DoctorRetrieveSerializer(serializers.ModelSerializer):
 
 
 class DoctorCreateUpdateSerializer(serializers.ModelSerializer):
-    avatar = serializers.ImageField(validators=[validators.validate_image_jpg_jpeg])
 
     class Meta:
         model = Doctor
@@ -60,6 +58,7 @@ class DoctorCreateUpdateSerializer(serializers.ModelSerializer):
             "medical_system_number",
             "appointment_fee",
         ]
+        extra_kwargs = {"avatar": {"validators": [validators.validate_image_jpg_jpeg]}}
 
     def validate(self, attrs):
         user_id = self.context["user_id"]
@@ -80,7 +79,6 @@ class DoctorCreateUpdateSerializer(serializers.ModelSerializer):
 
 
 class AdminDoctorCreateUpdateSerializer(serializers.ModelSerializer):
-    avatar = serializers.ImageField(validators=[validators.validate_image_jpg_jpeg])
 
     class Meta:
         model = Doctor
@@ -94,6 +92,7 @@ class AdminDoctorCreateUpdateSerializer(serializers.ModelSerializer):
             "medical_system_number",
             "appointment_fee",
         ]
+        extra_kwargs = {"avatar": {"validators": [validators.validate_image_jpg_jpeg]}}
 
     def validate(self, attrs):
         user_id = self.context["user_id"]
@@ -114,14 +113,65 @@ class AdminDoctorCreateUpdateSerializer(serializers.ModelSerializer):
 
 
 class SpecializationSerializer(serializers.ModelSerializer):
-    image = serializers.FileField(validators=[validators.validate_image_svg])
+    # image = serializers.FileField(validators=[validators.validate_image_svg])
 
     class Meta:
         model = Specialization
         fields = ["id", "name", "image"]
+        extra_kwargs = {"image": {"validators": [validators.validate_image_svg]}}
 
 
 class DoctorDegreeSerializer(serializers.ModelSerializer):
     class Meta:
         model = DoctorDegree
         fields = ["id", "degree"]
+
+
+class DoctorScheduleListRetrieveSerializer(serializers.ModelSerializer):
+    medical_center = serializers.CharField(source="medical_center.name")
+
+    class Meta:
+        model = DoctorSchedule
+        fields = ["medical_center", "day", "start_at", "end_at"]
+
+
+class DoctorScheduleCreateUpdateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = DoctorSchedule
+        exclude = ["doctor"]
+
+    def validate(self, attrs):
+        day = attrs.get("day")
+        medical_center_id = attrs.get("medical_center")
+        start_at = attrs.get("start_at")
+        end_at = attrs.get("end_at")
+
+        if DoctorSchedule.objects.filter(
+            doctor_id=self.context["user_id"],
+            medical_center_id=medical_center_id,
+            day=day,
+        ).exists():
+            raise serializers.ValidationError(
+                "برای مرکز درمانی مورد نظر قبلا روز کاری ثبت کرده اید"
+            )
+
+        schedule = MedicalCenterSchedule.objects.filter(
+            medical_center_id=medical_center_id, day=day
+        ).first()
+
+        if not schedule:
+            raise serializers.ValidationError(
+                "روز کاری انتخاب شده جزو روز کاری مرکز درمانی مورد نظر نیست"
+            )
+
+        if start_at < schedule.open_at or end_at > schedule.close_at:
+            raise serializers.ValidationError(
+                "زمان های انتخاب شده جزو ساعات کاری مرکز درمانی نمی باشد"
+            )
+
+        return super().validate(attrs)
+
+    def create(self, validated_data):
+        user_id = self.context["user_id"]
+        return DoctorSchedule.objects.create(**validated_data, doctor_id=user_id)
