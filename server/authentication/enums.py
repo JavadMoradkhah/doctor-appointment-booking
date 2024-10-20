53516bad7b602e560bbadfeb32c21aad1775fa94
Mohammad222PR <mohammades13851@gmail.com>
2024-10-21 00:56:40 +0330

    refactor: remove UrlTargetRole enum usage from authentication

--- server/authentication/enums.py
+++ server/authentication/enums.py
@@ -1,8 +1,3 @@
 from enum import Enum
-from account import choices
 
 
-class UrlTargetRole(Enum):
-    PATIENT = choices.USER_ROLE_PATIENT
-    DOCTOR = choices.USER_ROLE_DOCTOR
-    MANAGER = choices.USER_ROLE_MANAGER
