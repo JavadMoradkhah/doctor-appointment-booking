8b4453d6f3f5ec07631d155631051e932430dc26
Mohammad222PR <mohammades13851@gmail.com>
2024-10-21 00:40:55 +0330

    feat: create and register Otp and OtpBlacklist models

--- server/authentication/admin.py
+++ server/authentication/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Otp, OtpBlacklist
 
-# Register your models here.
+@admin.register(Otp)
+class OtpAdmin(admin.ModelAdmin):
+    list_display = ("phone", "code", "attempts", "first_attempt", "last_attempt", "expires_at")
+    search_fields = ("phone",)
+    list_filter = ("expires_at",)
+    readonly_fields = ("first_attempt", "last_attempt")
+
+@admin.register(OtpBlacklist)
+class OtpBlacklistAdmin(admin.ModelAdmin):
+    list_display = ("phone", "expires_at", "created_at")
+    search_fields = ("phone",)
+    list_filter = ("expires_at",)
+    readonly_fields = ("created_at",)
--- server/authentication/models.py
+++ server/authentication/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
+from account.validators import PhoneValidator
+
+
 # Create your models here.
+
+
+class Otp(models.Model):
+    phone = models.CharField(
+        max_length=11,
+        unique=True,
+        validators=[PhoneValidator()],
+        verbose_name="شماره موبایل",
+    )
+    code = models.CharField(max_length=255)
+    attempts = models.PositiveSmallIntegerField(default=1)
+    first_attempt = models.DateTimeField(auto_now_add=True)
+    last_attempt = models.DateTimeField(auto_now=True)
+    expires_at = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return f"Phone{self.phone}"
+
+class OtpBlacklist(models.Model):
+    phone = models.CharField(
+        max_length=11,
+        unique=True,
+        validators=[PhoneValidator()],
+        verbose_name="شماره موبایل",
+    )
+    expires_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return f"Phone{self.phone}"
+
